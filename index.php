97684a219abbce99746393c936cd5f7275210a2e
Juan Pablo Morales Rincon <jupmoralesri@unal.edu.co>
2021-01-24 14:08:24 -0500

    Cambiado algunos estilos

--- index.php
+++ index.php
@@ -151,59 +151,51 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Bree+Serif&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Sriracha&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/b94857a29d.js" crossorigin="anonymous"></script>
     <title>Explorador de archivos web</title>
 </head>
 <body>
-    <div class="container-fluid mb-4">
+    <div class="container">
         <div class="row">
-            <div class="col-md-12">
-                <div class="jumbotron bg-light" style="opacity:0.8;">
-                    <div class="container">
-                        <h1 class="display-4" style="opacity:1.0;"><strong>¡Bienvenido a WebHiker!</strong></h1>
-                        <p class="lead">Explorador web gráfico de archivos simple</p>
-                        <hr class="my-4">
-                        <p class="text-muted">Juan Pablo Morales Rincón</p>
-                        <?php if(isset($_GET["carpeta"])):?>
-                            <a class="btn btn-primary btn-sm" href="index.php" role="button">Volver a la raiz</a>
-                        <?php endif;?>
-                    </div>
-                </div>        
-            </div>
-            <div class="col-md-8 offset-md-2">
-                <ul class="nav nav-tabs" id="myTab" role="tablist">
-                    <li class="nav-item">
-                        <a class="nav-link active" style="color: black;" id="crear-tab" data-toggle="tab" href="#crear" role="tab" aria-controls="crear" aria-selected="true"><i class="fas fa-folder-plus fontf"></i> Crear Carpeta/Archivo</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link"  style="color: black;" id="renombrar-tab" data-toggle="tab" href="#renombrar" role="tab" aria-controls="renombrar" aria-selected="false"><i class="fas fa-edit fontf"></i> Renombrar</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" style="color: black;" id="copiar-tab" data-toggle="tab" href="#copiar" role="tab" aria-controls="copiar" aria-selected="false"><i class="fas fa-copy fontf"></i> Copiar/Pegar</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" style="color: black;" id="permisos-tab" data-toggle="tab" href="#permisos" role="tab" aria-controls="permisos" aria-selected="false"><i class="fas fa-edit fontf"></i> Cambiar Permisos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" style="color: black;" id="moverCortar-tab" data-toggle="tab" href="#moverCortar" role="tab" aria-controls="moverCortar" aria-selected="false"><i class="fas fa-edit fontf"></i> Mover/Cortar</a>
-                    </li>
-                </ul>
-                <div class="tab-content container" id="myTabContent">
-                    <div class="tab-pane fade show active text-justify mt-4" id="crear" role="tabpanel" aria-labelledby="crear-tab">Para crear una capeta o una archivo, solo basta con seleccinar la opción Crear carpeta/Crear fichero. Ten encuenta que al crear una carpeta, esta no debe contener espacios, también, si vas a crear un archivo, asegúrate de que contenga la extensión correspondiente (ejem: SoyUnArchivo.txt)</div>
-                    <div class="tab-pane fade text-justify mt-4" id="renombrar" role="tabpanel" aria-labelledby="renombrar-tab">Para renombrar un fichero (ya sea una carpeta o un archivo), tendrás que escribir el nombre del fichero a modificar y luego de un espacio, el nombre nuevo. Para los archivos, en el nuevo nombre también tendrás que especificar la extensión. Para las carpetas, el nuevo nombre no debe contener espacios</div>
-                    <div class="tab-pane fade text-justify mt-4" id="copiar" role="tabpanel" aria-labelledby="copiar-tab">Para copiar el contenido de un archivo de texto a otro, solo basta con escribir el nombre del archivo origen seguido de un espacio, luego la dirección del archivo de destino. Si el archivo de texto destino no existe, se creará uno con el nombre de este, con el contenido del arhivo del origen. (Para el nombre tanto del archivo de origen como de destino, especificar la extensión)</div>
-                    <div class="tab-pane fade text-justify mt-4" id="permisos" role="tabpanel" aria-labelledby="permisos-tab">Para cambiar los permisos de un fichero, bastará con escribir el nombre de este, seguido de un espacio, y luego los nuevos permisos en octal. (Ej: img 600). Ver documentacón de permisos de linux en octal siendo necesario.</div>
-                    <div class="tab-pane fade text-justify mt-4" id="moverCortar" role="tabpanel" aria-labelledby="moverCortar-tab">Para mover/cortar un fichero a otra carpeta, basta con escribir el nombre del archivo (con su extensión), seguido de un espacio, y luego la ruta de destino con un slash (/) que indicará el nuevo nombre. Ejemplo: hola.txt Prueba/hola.txt Esto moverá el archivo hola.txt a la carpeta Prueba con el mismo nombre, pero esto último es opcional, puedes colocar el nombre que quieras.</div>
-                </div>
-            </div>
-
+            <div class="col-md-12 text-center pt-5">
+                <h1 class="" style="opacity:1.0;"><strong>¡Bienvenido a WebHiker!</strong></h1>
+                <p class="lead">Explorador web gráfico de archivos simple by: <b class="text-muted">Juan Pablo Morales Rincón</b></p>
+                <?php if(isset($_GET["carpeta"])):?>
+                    <a class="btn btn-primary btn-sm" href="index.php" role="button">Volver a la raiz</a>
+                <?php endif;?>      
             <hr class="my-4">
+            </div>
         </div>
     </div>
-                        
-
     <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="row">                
+                    <div class="col-3">
+                        <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                            <a class="nav-link" style="color: black;" id="crear-tab" data-toggle="tab" href="#crear" role="tab" aria-controls="crear" aria-selected="true"><i class="fas fa-folder-plus fontf"></i> Crear Carpeta/Archivo</a>
+                            <a class="nav-link"  style="color: black;" id="renombrar-tab" data-toggle="tab" href="#renombrar" role="tab" aria-controls="renombrar" aria-selected="false"><i class="fas fa-edit fontf"></i> Renombrar</a>
+                            <a class="nav-link" style="color: black;" id="copiar-tab" data-toggle="tab" href="#copiar" role="tab" aria-controls="copiar" aria-selected="false"><i class="fas fa-copy fontf"></i> Copiar/Pegar</a>
+                            <a class="nav-link" style="color: black;" id="permisos-tab" data-toggle="tab" href="#permisos" role="tab" aria-controls="permisos" aria-selected="false"><i class="fas fa-edit fontf"></i> Cambiar Permisos</a>
+                            <a class="nav-link" style="color: black;" id="moverCortar-tab" data-toggle="tab" href="#moverCortar" role="tab" aria-controls="moverCortar" aria-selected="false"><i class="fas fa-edit fontf"></i> Mover/Cortar</a>
+                        </div>
+                    </div>
+                    <div class="col-9">
+                        <div class="tab-content" id="v-pills-tabContent">
+                            <div class="tab-pane fade text-justify mt-2 show active" id="crear" role="tabpanel" aria-labelledby="crear-tab">Para crear una capeta o una archivo, solo basta con seleccinar la opción Crear carpeta/Crear fichero. Ten encuenta que al crear una carpeta, esta no debe contener espacios, también, si vas a crear un archivo, asegúrate de que contenga la extensión correspondiente (ejem: SoyUnArchivo.txt)</div>
+                            <div class="tab-pane fade text-justify mt-2" id="renombrar" role="tabpanel" aria-labelledby="renombrar-tab">Para renombrar un fichero (ya sea una carpeta o un archivo), tendrás que escribir el nombre del fichero a modificar y luego de un espacio, el nombre nuevo. Para los archivos, en el nuevo nombre también tendrás que especificar la extensión. Para las carpetas, el nuevo nombre no debe contener espacios</div>
+                            <div class="tab-pane fade text-justify mt-2" id="copiar" role="tabpanel" aria-labelledby="copiar-tab">Para copiar el contenido de un archivo de texto a otro, solo basta con escribir el nombre del archivo origen seguido de un espacio, luego la dirección del archivo de destino. Si el archivo de texto destino no existe, se creará uno con el nombre de este, con el contenido del arhivo del origen. (Para el nombre tanto del archivo de origen como de destino, especificar la extensión)</div>
+                            <div class="tab-pane fade text-justify mt-2" id="permisos" role="tabpanel" aria-labelledby="permisos-tab">Para cambiar los permisos de un fichero, bastará con escribir el nombre de este, seguido de un espacio, y luego los nuevos permisos en octal. (Ej: img 600). Ver documentacón de permisos de linux en octal siendo necesario.</div>
+                            <div class="tab-pane fade text-justify mt-2" id="moverCortar" role="tabpanel" aria-labelledby="moverCortar-tab">Para mover/cortar un fichero a otra carpeta, basta con escribir el nombre del archivo (con su extensión), seguido de un espacio, y luego la ruta de destino con un slash (/) que indicará el nuevo nombre. Ejemplo: hola.txt Prueba/hola.txt Esto moverá el archivo hola.txt a la carpeta Prueba con el mismo nombre, pero esto último es opcional, puedes colocar el nombre que quieras.</div>
+                        </div>
+                    </div>
+                </div>
+                <hr class="my-4">
+            </div>
+        </div>
         <div class="row">
             <div class="col-md-12"> 
                 <div class="row">
@@ -287,7 +279,7 @@
                                 <img src="img/<?php echo (is_dir($path.'/'.$directorio)) ? 'descarga.png': 'archivo.png'?>" class="card-img-top" alt="...">
                                 <div class="card-body">
                                     <h5 class="card-title"><?php echo $directorio?></h5>
-                                    <p class="card-text">Propietario: Juan Pablo Morales R. <!--<?php // echo $propietarioNombre;?> --></p>
+                                    <p class="card-text">Propietario: Juan Pablo Morales <!--<?php // echo $propietarioNombre;?> --></p>
                                 </div>
                                 <ul class="list-group list-group-flush">
                                     <div class="dropdown mr-1 list-group-item">
@@ -304,13 +296,13 @@
                                 </ul>
                                 <div class="card-body">
                                     <?php if(is_dir($path.'/'.$directorio) && !$esTxt):?>
-                                        <a href="<?php echo 'index.php?carpeta='.$ruta.$directorio?>" class="btn btn-secondary btn-block">Navegar</a>
+                                        <a href="<?php echo 'index.php?carpeta='.$ruta.$directorio?>" class="btn btn-success btn-block">Navegar</a>
                                     <?php elseif ($esTxt):?>
-                                        <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#archivoModal<?php echo $contModal;?>">Ver contenido</button>
-                                        <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#editarContenido<?php echo $contModal;?>">Editar</button>
+                                        <button type="button" class="btn btn-dark" data-toggle="modal" data-target="#archivoModal<?php echo $contModal;?>">Ver contenido</button>
+                                        <button type="button" class="btn btn-info" data-toggle="modal" data-target="#editarContenido<?php echo $contModal;?>">Editar</button>
 
                                         <div class="modal fade" id="archivoModal<?php echo $contModal;?>" tabindex="-1" role="dialog" aria-labelledby="archivoModalLabel<?php echo $contModal;?>" aria-hidden="true">
-                                        <div class="modal-dialog modal-dialog-scrollable" role="document">
+                                        <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered" role="document">
                                             <div class="modal-content">
                                             <div class="modal-header">
                                                 <h5 class="modal-title" id="archivoModalLabel<?php echo $contModal;?>"><?php echo $directorio?></h5>
@@ -326,7 +318,7 @@
                                         </div>
                                         
                                         <div class="modal fade" id="editarContenido<?php echo $contModal;?>" tabindex="-1" role="dialog" aria-labelledby="editarArchivoModalLabel<?php echo $contModal;?>" aria-hidden="true">
-                                            <div class="modal-dialog modal-dialog-scrollable" role="document">
+                                            <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered" role="document">
                                                     <div class="modal-content">
                                                     <div class="modal-header">
                                                         <h5 class="modal-title" id="editarArchivoModalLabel<?php echo $contModal;?>"><?php echo $directorio?></h5>
@@ -339,13 +331,17 @@
                                                             <div class="form-group">
                                                                 <input type="hidden" name="nombreArchivo" value="<?php echo $directorio?>">
                                                                 <label for="message-text" class="col-form-label">Editar/Agregar contenido:</label>
-                                                                <textarea class="form-control" id="message-text" name="contenido" style="height:200px"><?php echo $archivo;?></textarea>
+                                                                <textarea class="form-control" id="message-text" name="contenido" style="height:300px"><?php echo $archivo;?></textarea>
                                                             </div>
-                                                            <button type="submit" class="btn btn-secondary btn-block" name="modificar">Confirmar</button>
+                                                            <div class="form-row">
+                                                                <div class="col">
+                                                                    <button type="submit" class="btn btn-success btn-block" name="modificar">Confirmar</button>
+                                                                </div>
+                                                                <div class="col">
+                                                                    <button type="submit" class="btn btn-danger btn-block" data-dismiss="modal">Cancelar</button>
+                                                                </div>
+                                                            </div>
                                                         </form>
-                                                    </div>
-                                                    <div class="modal-footer">
-                                                        <button type="submit" class="btn btn-danger btn-block" data-dismiss="modal">Cancelar</button>
                                                     </div>
                                                 </div>
                                             </div>
@@ -376,6 +372,7 @@
     <style>
         body{
             background-image: url("img/bg7.png");
+            font-family: 'Bree Serif', serif;
             
         }
         .not-allowed{
@@ -391,9 +388,9 @@
             opacity: 1.0;
         }
 
-        .jumbotron{
-            border-bottom: 2px solid #5B5958 !important;
-            font-family: 'Sriracha', cursive;
+        a.active{ 
+            background-color: black !important;
+            color: white !important;
         }
 
         .nav-item:hover{
