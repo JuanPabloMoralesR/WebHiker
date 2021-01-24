0aad8fb78e8cdfa72cedaae93458db0db10933d5
Juan Pablo Morales Rincon <jupmoralesri@unal.edu.co>
2021-01-24 13:12:02 -0500

    Actualizados cdn de bootstrap

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     $cont = 1;
     $contModal = 0;
     // --------- Obtener ficheros de una ruta ---------
-    $directorios = array();
+    $directorios = [];
     $dir = dir($path);
     while (false !== ($entry = $dir->read())) {
         if ($entry != '.' && $entry != '..') {
@@ -25,7 +25,7 @@
         }
     }
 
-    $informes = array();
+    $informes = [];
     // --------- Obtener petición de realizar alguna acción ---------
     if (isset($_POST['enviar']) && $_POST['varDestino'] !== "") {
             $varDir = $_POST['varDestino'];
@@ -153,7 +153,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Sriracha&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/b94857a29d.js" crossorigin="anonymous"></script>
-    <title>Document</title>
+    <title>Explorador de archivos web</title>
 </head>
 <body>
     <div class="container-fluid mb-4">
@@ -368,10 +368,10 @@
         </div>
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <script src="https://unpkg.com/@popperjs/core@2/dist/umd/popper.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+
 </body>
     <style>
         body{
